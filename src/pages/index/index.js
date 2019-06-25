6c3c64e2d7be632d6e6fe24792b8068918d59b3c
Liu Liu <liuliu@mofanghr.com>
2019-06-25 21:11:43 +0800

    update

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '东军的小玩意',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
